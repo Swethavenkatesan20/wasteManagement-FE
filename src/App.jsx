2e5714149f15b820de0773077c32347c95dc5f59
Swethavenkatesan20 <157340837+Swethavenkatesan20@users.noreply.github.com>
2024-05-23 00:44:36 +0530

    Update App.jsx
    completely updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import Landing from './pages/Landing'
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import LoginWrapper from "./components/LoginWrapper";
+import DashboardWrapper, { loader as userLoader } from "./components/DashboardWrapper";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import { loader as profileLoader } from './pages/Profile';
+import HarmfulEffects from "./pages/HarmfulEffects";
+import ReviewsPage from "./pages/ReviewsPage";
+import Waste from "./pages/Waste";
+
 
 
 
@@ -25,9 +33,38 @@ const router=createBrowserRouter([
         element: <Login/>
       }
     ]
-    
-    
-  }
+  },
+  {
+    path: 'dashboard',
+    element: <DashboardWrapper/>,
+    loader: userLoader,
+    children: [
+      {
+        path: '',
+        element: <Dashboard/>
+        
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+        loader: profileLoader
+        
+      },
+      {
+        path: 'wastedisposal',
+        element: <Waste />
+      },
+      {
+        path: 'effects',
+        element: <HarmfulEffects />
+      },
+      {
+        path: 'reviews',
+        element: <ReviewsPage /> 
+      }
+      
+    ]
+  },
 
 ]);
 
